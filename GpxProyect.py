05169aa81aa4681030ee610254d76c4ffb820518
[Julian Lopez] <[juguilopez@gmail.com]>
2026-02-14 21:27:31 -0500

    Primer commit

---
+++ GpxProyect.py
@@ -0,0 +1,9 @@
+#Se tiene un archivo GPX con la ruta de una persona en su recorrido por la zona turística.
+# La persona tomó varias fotografías y las almacenó en su celular cuidando que la metadata 
+# tuviese la ubicación (EXIF).
+
+#Elaborar un programa en Python que analice el archivo GPX, y lo visualice y muestre cada una de 
+# las fotografías en el  lugar del mapa que presenta el sitio donde fue tomada.
+
+#La visualización debe tener un mapa 2D y un mapa 3D.
+
